b786a9e46622b87a1fb6a3c26097e8d2207571e4
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2015-11-13 22:51:06 -0800

    Changes

--- js/discovery.js
+++ js/discovery.js
@@ -1,15 +1,9 @@
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 	$('ul.tabs').tabs('select_tab', 'tab_id');
-	
-    function turnGray() {
-    	if ($(this).hasClass("white")) {
-    		$(this).removeClass("white").addClass("grey");
-    	}
 
-    	else {
-    		$(this).removeClass("gray").addClass("white");
-    	}   	
+    function turnGray() {
+    	$(this).addClass("grey");
     };
 });
 
