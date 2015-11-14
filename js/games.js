70ee17e2c0127fe5e4c68c01d64fa11b460c3f79
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2015-11-13 23:29:12 -0800

    Tests

---
+++ js/games.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ 	$(".button-collapse").sideNav();
+ 	$('.modal-trigger').leanModal();
+
+ 	$('.slider').slider({
+    	indicators: false,
+    	transition: 500
+    });
+});
